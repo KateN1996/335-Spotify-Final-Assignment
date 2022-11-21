fd2e4fe358555ddb0b6ef63f9583cabbc89ba44d
Kevin Nisterenko <kevin@nisterenko.com>
2022-11-20 22:50:07 -0700

    Removed my old ugly code

--- Main.java
+++ Main.java
@@ -2,19 +2,5 @@ import javax.swing.*;
 public class Main {  
     public static void main(String[] args) {  
     	BrazilBeatsUI gui = new BrazilBeatsUI();
-    	
-    	
-    	/*
-        JFrame f=new JFrame();//creating instance of JFrame  
-                
-        JButton b=new JButton("click");//creating instance of JButton  
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
-                
-        f.add(b);//adding button in JFrame  
-                
-        f.setSize(400,500);//400 width and 500 height  
-        f.setLayout(null);//using no layout managers  
-        f.setVisible(true);//making the frame visible  
-        */
     }  
 }  
