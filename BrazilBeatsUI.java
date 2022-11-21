1bfdd70128e48e83291de8c2fdaf7aeb587371a6
raycauski <85911092+raycauski@users.noreply.github.com>
2022-11-20 21:19:49 -0700

    Playback Bar - Spotify Pirated from Depths of Brasilia

--- BrazilBeatsUI.java
+++ BrazilBeatsUI.java
@@ -1,9 +1,7 @@
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Locale;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -21,7 +19,8 @@ public class BrazilBeatsUI {
 	//private Color appColor = new Color(130, 150, 180);
 	//private Color detailColor = new Color(70, 70, 70);
 	private Color appColor = new Color(50, 55, 50);
-	private Color bgColor = new Color(40, 44, 40);
+	private Color borderColor = new Color(30, 33, 30);
+	private Color barColor = new Color(40, 44, 40);
 	private Color accentColor = new Color(30, 80, 30);
 	private Color detailColor = new Color(230, 220, 100);
 	
@@ -40,10 +39,8 @@ public class BrazilBeatsUI {
 		panel.setBackground(appColor);
 		panel.setFocusable(true);
 		
-		// Navigation Menu
-		Container navigationMenu = createNavigationContainer();
-		// Current Song Preview and info
-		Container songPreview = createSongPreviewContainer();
+
+
 		// Library view
 		Container libraryView = createLibraryViewContainer();
 		// Playback Bar
@@ -55,41 +52,51 @@ public class BrazilBeatsUI {
 		GridBagConstraints gbc = new GridBagConstraints();
 		
 		JPanel navPanel = new JPanel(new GridBagLayout());
-		navPanel.setBackground(bgColor);
+		navPanel.setBackground(borderColor);
 		gbc.fill = GridBagConstraints.VERTICAL;
-		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.anchor = GridBagConstraints.SOUTHWEST;
 		gbc.weightx = 1;
 		gbc.weighty = 1;
 		gbc.gridx = 0;
-		gbc.gridy = 0;
+		gbc.gridy = 1;
 		panel.add(navPanel, gbc);
 		
-		navPanel.add(navigationMenu, gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		gbc.anchor = GridBagConstraints.SOUTHWEST;
+		
+		// Current Song Preview and info
+		Container songPreview = createSongPreviewContainer();
 		gbc.insets = new Insets(5,10,5,10);
+		gbc.fill = GridBagConstraints.NONE;
 		navPanel.add(songPreview, gbc);
 		
+		// Navigation Menu
+		Container navigationMenu = createNavigationContainer();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+	
+		navPanel.add(navigationMenu, gbc);
+		
+		//gbc.anchor = GridBagConstraints.NORTH;
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		//panel.add(libraryView, gbc);
 		
 		
+		JPanel playbackPanel = new JPanel();
+		playbackPanel.setBackground(barColor);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(0,0,0,0);
+		playbackPanel.add(playbackBar, gbc);
 		
-		
-		/*
-		gbc.anchor = GridBagConstraints.SOUTHWEST;
+		gbc.anchor = GridBagConstraints.SOUTH;
 		gbc.gridx = 0;
 		gbc.gridy = 1;
-		//panel.add(songPreview, gbc);
-		gbc.gridx = 1;
-		gbc.gridy = 0;
-		panel.add(libraryView, gbc);
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		panel.add(playbackBar, gbc);
+		panel.add(playbackPanel, gbc);
+		
+		//gbc.anchor = GridBagConstraints.NORTHEAST;
 		gbc.gridx = 2;
 		gbc.gridy = 0;
-		panel.add(brazilBeatsView, gbc);
-		*/
+		//panel.add(brazilBeatsView, gbc);
 		
 		
 		// Set and open frame
@@ -112,47 +119,46 @@ public class BrazilBeatsUI {
 		Container navContainer = new Container();
 		GridBagLayout colLayout = new GridBagLayout();
 		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.fill = GridBagConstraints.HORIZONTAL;
+		
 		navContainer.setLayout(colLayout);
 		
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(5,10,5,10);
 		// Create components
 		JButton libraryButton = new JButton("Your Library");
 		libraryButton.setFont(mainFont);
 		libraryButton.setForeground(detailColor);
 		libraryButton.setBackground(accentColor);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		navContainer.add(libraryButton, gbc);
 		
 		JButton searchButton = new JButton("Search");
 		searchButton.setFont(mainFont);
 		searchButton.setForeground(detailColor);
 		searchButton.setBackground(accentColor);
+		gbc.gridy = 1;
+		navContainer.add(searchButton, gbc);
 		
 		JLabel playlistsLabel = new JLabel("Playlists");
 		playlistsLabel.setFont(headerFont);
 		playlistsLabel.setForeground(detailColor);
+		gbc.gridy = 2;
+		navContainer.add(playlistsLabel, gbc);
 		
-		String[] examplePlaylists = new String[] {"Drake playlist", "Logic isn't that bad", "6ix9ine hidden gems", "YEAT's greatest hits", "Wolfgang Amadeus Mozart"};
+		
+		// TODO: Change this to actual playlist object array
+		String[] examplePlaylists = new String[] {"Drake playlist", "Logic isn't that bad", "6ix9ine hidden gems", "YEAT's greatest hits", "Wolfgang Amadeus Mozart", "Dababy sleep playlist 2"};
 		JList<String> playlistsList = new JList<String>(examplePlaylists);					// Store an arrayList of Playlist Objects here
-		playlistsList.setBackground(bgColor);
+		playlistsList.setBackground(borderColor);
 		playlistsList.setForeground(detailColor);
 		playlistsList.setFont(captionFont);
 		JScrollPane playlistScroll = new JScrollPane();
 		playlistScroll.setViewportView(playlistsList);
 		playlistsList.setLayoutOrientation(JList.VERTICAL);
 		//JList<Playlist> playlistsList = new JList<Playlist>();		
-		
-		
-		// Add all components to container
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.insets = new Insets(5,10,5,10);
-		navContainer.add(libraryButton, gbc);
-		gbc.gridy = 1;
-		navContainer.add(searchButton, gbc);
-		gbc.gridy = 2;
-		navContainer.add(playlistsLabel, gbc);
 		gbc.gridy = 3;
 		navContainer.add(playlistsList, gbc);
-	
 		
 		return navContainer;
 	}
@@ -165,41 +171,40 @@ public class BrazilBeatsUI {
 	private Container createSongPreviewContainer() {
 		Container songContainer = new Container();
 		GridBagLayout colLayout = new GridBagLayout();
-		GridBagConstraints gbc = new GridBagConstraints();
+		
 		songContainer.setLayout(colLayout);
 		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(5,10,5,10);
+		gbc.anchor = GridBagConstraints.WEST;
 		// Create components
 		// TESTING ALBUM COVER -- HARDCODED   ---- 
 		albumImagePreview = new JLabel("");
-		
-		songTitlePreview = new JLabel("");
-		songTitlePreview.setFont(mainFont);
-		songTitlePreview.setForeground(detailColor);
-		
-		albumTitlePreview = new JLabel("");
-		albumTitlePreview.setFont(captionFont);
-		albumTitlePreview.setForeground(detailColor);
-		
-		artistTitlePreview = new JLabel("");
-		artistTitlePreview.setFont(captionFont);
-		artistTitlePreview.setForeground(detailColor);
-		
-		updateSongPreview();
-		
-		gbc.insets = new Insets(5,10,5,10);
-		gbc.anchor = GridBagConstraints.WEST;
-		// Add all components to container
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		gbc.weightx = 1;
 		songContainer.add(albumImagePreview, gbc);
+		
+		songTitlePreview = new JLabel("");
+		songTitlePreview.setFont(mainFont);
+		songTitlePreview.setForeground(detailColor);
 		gbc.gridy = 2;
 		songContainer.add(songTitlePreview, gbc);
+		
+		albumTitlePreview = new JLabel("");
+		albumTitlePreview.setFont(captionFont);
+		albumTitlePreview.setForeground(detailColor);
 		gbc.gridy = 3;
 		songContainer.add(albumTitlePreview, gbc);
+		
+		artistTitlePreview = new JLabel("");
+		artistTitlePreview.setFont(captionFont);
+		artistTitlePreview.setForeground(detailColor);
 		gbc.gridy = 4;
 		songContainer.add(artistTitlePreview, gbc);
 		
+		updateSongPreview();
+
 		return songContainer;
 	}
 	
@@ -213,8 +218,77 @@ public class BrazilBeatsUI {
 	
 	private Container createPlaybackOptionsContainer() {
 		Container playbackContainer = new Container();
-		BoxLayout colLayout = new BoxLayout(playbackContainer, BoxLayout.Y_AXIS);
-		playbackContainer.setLayout(colLayout);
+		GridBagLayout rowLayout = new GridBagLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
+		playbackContainer.setLayout(rowLayout);
+		
+		gbc.insets = new Insets(5,10,5,10);
+		gbc.anchor = GridBagConstraints.SOUTH;
+	
+		JLabel timeStampCur = new JLabel("0:00");
+		timeStampCur.setFont(captionFont);
+		timeStampCur.setForeground(detailColor);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		playbackContainer.add(timeStampCur, gbc);
+		
+		JProgressBar playbackBar = new JProgressBar(SwingConstants.HORIZONTAL);
+		playbackBar.setPreferredSize(new Dimension(400, 12));
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		playbackContainer.add(playbackBar, gbc);
+		gbc.fill = GridBagConstraints.NONE;
+		
+		JLabel timeStampEnd = new JLabel("2:52");
+		timeStampEnd.setFont(captionFont);
+		timeStampEnd.setForeground(detailColor);
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		playbackContainer.add(timeStampEnd, gbc);
+		
+		
+		Container buttonContainer = new Container();
+		Dimension buttonSpacing = new Dimension(10,0);
+		buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.X_AXIS));
+		
+		JButton shuffleButton = new JButton("$");
+		shuffleButton.setBackground(accentColor);
+		shuffleButton.setForeground(detailColor);
+		shuffleButton.setFont(mainFont);
+		buttonContainer.add(shuffleButton);
+		buttonContainer.add(Box.createRigidArea(buttonSpacing));
+		
+		JButton restartButton = new JButton("|<");
+		restartButton.setBackground(accentColor);
+		restartButton.setForeground(detailColor);
+		restartButton.setFont(mainFont);
+		buttonContainer.add(restartButton);
+		buttonContainer.add(Box.createRigidArea(buttonSpacing));
+		
+		JButton playPauseButton = new JButton("||");
+		playPauseButton.setBackground(accentColor);
+		playPauseButton.setForeground(detailColor);
+		playPauseButton.setFont(mainFont);
+		buttonContainer.add(playPauseButton);
+		buttonContainer.add(Box.createRigidArea(buttonSpacing));
+		
+		JButton skipButton = new JButton(">|");
+		skipButton.setBackground(accentColor);
+		skipButton.setForeground(detailColor);
+		skipButton.setFont(mainFont);
+		buttonContainer.add(skipButton);
+		buttonContainer.add(Box.createRigidArea(buttonSpacing));
+		
+		JButton saveButton = new JButton(":)");
+		saveButton.setBackground(accentColor);
+		saveButton.setForeground(detailColor);
+		saveButton.setFont(mainFont);
+		buttonContainer.add(saveButton);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		playbackContainer.add(buttonContainer, gbc);
 		
 		return playbackContainer;
 	}
@@ -227,6 +301,9 @@ public class BrazilBeatsUI {
 		return brazilContainer;
 	}
 	
+	/**
+	 * Updates the song preview view to match current song's Album art, title, album name, and artist name
+	 */
 	private void updateSongPreview() {
 		File albumImageFile = new File("Mmfood.jpg");
 		Image albumImage;
