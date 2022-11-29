fa0eab5aa11f83078081dea6b8b35ec425a54785
Kevin Nisterenko <kevin@nisterenko.com>
2022-11-29 09:37:02 -0700

    Better Example Showcase

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;  
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 public class Main {  
 
     static final Dimension defaultRes = new Dimension(1400, 800);
@@ -13,10 +16,23 @@ public class Main {
 
         JFrame frame = new JFrame("Brazil Beats Premium");
 
-		String[] lst = {"Set on You", "Come and Get your Love", "Country Roads (Take me Home)", 
-						"Material Girl", "Exagerado", "Highway to Hell", "Friday", "Merry go Round of life",
-					    "Hooked on a feeling", "Minecraft Theme Song"};
-        JPanel panel = new PlaylistPanel("Random Music", lst);
+		// String[] lst = {"Set on You", "Come and Get your Love", "Country Roads (Take me Home)", 
+		// 				"Material Girl", "Exagerado", "Highway to Hell", "Friday", "Merry go Round of life",
+		// 			    "Hooked on a feeling", "Minecraft Theme Song"};
+        // JPanel panel = new PlaylistPanel("Random Music", lst);
+
+		File albumImageFile = new File("Mmfood.jpg");
+		Image albumImage = null;
+		try {
+			albumImage = ImageIO.read(albumImageFile);
+			albumImage = albumImage.getScaledInstance(IMG_RES_MAX, IMG_RES_MAX, Image.SCALE_DEFAULT);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		JPanel panel = new PlayerPanel(albumImage);
+
         // Set and open frame
 		frame.setSize(defaultRes);
 		frame.add(panel);
