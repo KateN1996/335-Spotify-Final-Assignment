abb553f7cf627b069f55211dbae61b83e87e7ef6
Ryan Pecha <ryanpecha@aol.com>
2022-11-29 05:11:16 -0700

    found that soundcloud isnt authenticating new apps :(

--- SourceInterface_SoundCloud.java
+++ SourceInterface_SoundCloud.java
@@ -1,10 +1,34 @@
 import java.util.ArrayList;
 
+import de.voidplus.soundcloud.*;
+
 public class SourceInterface_SoundCloud extends SourceInterface {
 
+	public SoundCloud soundCloud;
+	
+	// https://github.com/nok/soundcloud-java-library#api
+	// https://docs.google.com/forms/d/e/1FAIpQLSfNxc82RJuzC0DnISat7n4H-G7IsPQIdaMpe202iiHZEoso9w/closedform
+	public SourceInterface_SoundCloud() {
+		/*
+		soundCloud = new SoundCloud(
+		    "appClientId",
+		    "appClientSecret"
+		);
+		*/
+	}
+	
 	@Override
 	public ArrayList<SearchResult> search(String searchText) {
-		// TODO Auto-generated method stub
+		/*
+		ArrayList<Track> result = soundCloud.findTrack("Chromatics");
+		if(result!=null){
+		    System.out.println("Tracks: "+result.size());
+		    for(Track track:result){
+		        System.out.println(track);
+		    }
+		}
+		
+		*/
 		return null;
 	}
 
