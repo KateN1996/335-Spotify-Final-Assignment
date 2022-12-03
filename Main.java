ea9036a4d5121a4c6556bfd0f0d69c15b2861187
Ryan Pecha <ryanpecha@aol.com>
2022-12-02 19:26:21 -0700

    m

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import javax.swing.*;  
 public class Main {  
     public static void main(String[] args) {  
-      MusicPlayer spotify = new MusicPlayer();
+    	//MusicPlayer spotify = new MusicPlayer();
     	BrazilBeatsUI gui = new BrazilBeatsUI();
     }  
 }  
