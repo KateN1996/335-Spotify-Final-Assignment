369ef7edc46665274cac742d35f16a3512c24b13
KateN1996 <98198608+KateN1996@users.noreply.github.com>
2022-12-06 18:46:34 -0700

    Comments

--- SongPlayer.java
+++ SongPlayer.java
@@ -12,27 +12,30 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * 
- * 
- * @author Kate
+ * @author Kate Nixon
+ *
+ * SongPlayer class uses Java's swing to play
+ * wav files. Wav files are played using a 
+ * conjuction of currentFile to get currentClip
+ * which represents the song itself
+ * This class also keeps track of the current
+ * frame, the statuss of the song (playing or paused)
+ * and the volume
  */
 public class SongPlayer {
-
-	
-	
 	// current play data
 	private File currentFile;
 	private Song currentSong;
-    private Clip currentClip;
-    private Long currentFrame;
-    private AudioInputStream currentAudioInputStream;
-    private boolean paused;
-    private FloatControl currentGainControl;
-    private Float currentGain;
-
-    
+	private Clip currentClip;
+    	private Long currentFrame;
+    	private AudioInputStream currentAudioInputStream;
+    	private boolean paused;
+    	private FloatControl currentGainControl;
+    	private Float currentGain;
 	
-	/*
+	/**
 	 * constructor
+	 * starts off with no song
 	 */
 	public SongPlayer() {
 		currentFile = null;
@@ -46,8 +49,11 @@ public class SongPlayer {
 	
 	
 	
-	/*
+	/**
 	 * plays the given Song
+	 * if the song is null, then return without playing anyting
+	 * if song is the same as current song, then it restarts
+	 * otherwise play the song passed in
 	 */
 	public void play(Song song) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 		if (song == null) {
@@ -92,8 +98,10 @@ public class SongPlayer {
 	
 	
 	
-	/*
-	 * pauses the currently playing song
+	/**
+	 * This pauses the currently playing song
+	 * makes sure that the current song exists
+	 * and isn't already paused.
 	 */
 	public void pause() {
 				
@@ -108,10 +116,8 @@ public class SongPlayer {
 		paused = true;
 	}
 	
-	
-	
-	/*
-	 * resumes the currently paused song
+	/**
+	 * if the current song is paused, it resumes playing it
 	 */
 	public void resume() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 		if (currentSong == null) {
@@ -121,36 +127,32 @@ public class SongPlayer {
 			return;
 		}
 		currentClip.close();
-        resetAudioStream();
-        currentClip.setMicrosecondPosition(currentFrame);
-        this.currentClip.start();
+        	resetAudioStream();
+        	currentClip.setMicrosecondPosition(currentFrame);
+        	this.currentClip.start();
 		paused = false;
 	}
 	
-	
-	
-	/*
+	/**
 	 * sets the current time of the song in microseconds
+	 * @param Long time r4epresents the time that the song needs to be set to
 	 */
 	public void setTime(Long time) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 		if (currentSong == null) {
 			return;
 		}
-		if (time > 0 && time < currentClip.getMicrosecondLength()) 
-        {
+		if (time > 0 && time < currentClip.getMicrosecondLength()) {
 			closeCurrentSong();
-            resetAudioStream();
-            currentClip.stop();
-            currentFrame = time;
-            currentClip.setMicrosecondPosition(time);
-            currentClip.start();
-        }
+            	resetAudioStream();
+            	currentClip.stop();
+            	currentFrame = time;
+            	currentClip.setMicrosecondPosition(time);
+            	currentClip.start();
+        	}
 	}
 	
-	
-	
-	/*
-	 * 
+	/**
+	 * restarts the current song, if it is not null
 	 */
 	public void restartCurrentSong() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 		if (currentSong == null) {
@@ -163,10 +165,8 @@ public class SongPlayer {
 		currentClip.start();
 	}
 	
-	
-	
-	/*
-	 * 
+	/**
+	 * stops the current song if it is not null and closes it
 	 */
 	private void closeCurrentSong() {
 		if (currentSong == null) {
@@ -177,13 +177,11 @@ public class SongPlayer {
 		}
 		paused = false;
 		currentFrame = 0L;
-        currentClip.stop();
-        currentClip.close();
+        	currentClip.stop();
+        	currentClip.close();
 	}
 	
-	
-	
-    /*
+    /**
      *  Method to reset audio stream
      */
     private void resetAudioStream() throws UnsupportedAudioFileException, IOException,  LineUnavailableException {
@@ -194,10 +192,8 @@ public class SongPlayer {
         currentGainControl.setValue(currentGain);
     }
     
-    
-    
-    /*
-     * gets the current time of the current song in microseconds 
+    /**
+     * return gets the current time of the current song in microseconds 
      */
     public long getCurrentTimeMicroseconds() {
     	if (currentSong == null) {
@@ -206,9 +202,7 @@ public class SongPlayer {
     	return currentClip.getMicrosecondPosition();
     }
     
-    
-    
-    /*
+    /**
      * gets the length of the current song in microseconds
      */
     public long getCurrentSongLength() {
@@ -218,28 +212,22 @@ public class SongPlayer {
     	return currentClip.getMicrosecondLength();
     }
     
-    
-    
-    /*
+    /**
      * returns the float amplitude in range 0,1
      */
     public float getCurrentAmplitude() {
     	return currentClip.getLevel();
     }
     
-    
-    
-    /*
-     * 
+    /**
+     * returns the current song
      */
     public Song getCurrentSong() {
     	return this.currentSong;
     }
-	
-    
-    
-    /*
-     * 
+
+    /**
+     * increments the volume based on amount passed in
      */
     public void incrementGain(float increment) {
     	this.currentGain += increment;
@@ -247,64 +235,39 @@ public class SongPlayer {
     }
     
     
-    
-    /*
-     * 
+    /**
+     * sets the volume to the gain
      */
     public void setGain(float gain) {
     	this.currentGain = gain;
     	this.currentGainControl.setValue(this.currentGain);
     }
-    
-    
-    /*
-     * 
+  
+    /**
+     * returns the max volume
      */
     public float getGainMax() {
     	return currentGainControl.getMaximum();
     }
     
-    
-    
-    /*
-     * 
+    /**
+     * returns sthe min volume
      */
     public float getGainMin() {
     	return currentGainControl.getMinimum();
     }
     
-    
-    
-    /*
-     * 
+    /**
+     * gets the audio input stream
      */
-    /*
-    public int getSampleInt(int sampleNumber) {
-
-        if (sampleNumber < 0 || sampleNumber >= data.length / sampleSize) {
-            throw new IllegalArgumentException(
-                    "sample number can't be < 0 or >= data.length/"
-                            + sampleSize);
-        }
-
-        byte[] sampleBytes = new byte[4]; //4byte = int
-
-        for (int i = 0; i < sampleSize; i++) {
-            sampleBytes[i] = data[sampleNumber * sampleSize * channelsNum + i];
-        }
-
-        int sample = ByteBuffer.wrap(sampleBytes)
-                .order(ByteOrder.LITTLE_ENDIAN).getInt();
-        return sample;
-    }
-    */
-    
     public AudioInputStream getAudioInputStream(){
     	return currentAudioInputStream;
     }
+  
+    /**
+    *returns the currentClip, the media of the song
+    */
     public Clip getCurrentClip(){
     	return currentClip;
     }
 }
-
-
